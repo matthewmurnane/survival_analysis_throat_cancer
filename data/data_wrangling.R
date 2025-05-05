2e41287016822111c768f66caddf6adfbebf203f
matthewmurnane <mmurnane00@gmail.com>
2025-05-05 11:54:59 -0700

    data wrangling

--- data/data_wrangling.R
+++ data/data_wrangling.R
@@ -5,7 +5,9 @@ pharynx <- read_excel("data/pharynx.xls")
 
 glimpse(pharynx)
 
-# Changing variable types
+
+# Changing Variable Types -------------------------------------------------
+
 pharynx$Case <- as.character(pharynx$Case)
 pharynx$Inst <- as.character(pharynx$Inst)
 pharynx$Sex <- as.factor(pharynx$Sex)
@@ -17,13 +19,23 @@ pharynx$T_Stage <- as.factor(pharynx$T_Stage)
 pharynx$N_Stage <- as.factor(pharynx$N_Stage)
 pharynx$Status <- as.factor(pharynx$Status)
 
-#Fixing Entry_dt
+# fixing the date of entry variable ---------------------------------------
 
 year <- pharynx$Entry_Dt %% 100           # Get last two digits
 day <- pharynx$Entry_Dt %/% 100           # Get the day of the year
 year_full <- 1900 + year
 
-# Convert to actual dates
 pharynx$Entry_Dt <- as.Date(day - 1, origin = paste0(year_full, "-01-01"))
 
+
+# Missing values Remove ---------------------------------------------------
+
+missing_rows <- pharynx$Cond == "9" | pharynx$Grade == "9"
+
+pharynx <- pharynx[!missing_rows,]
+
+# Finishing up ------------------------------------------------------------
+
 glimpse(pharynx)
+
+write.csv(pharynx, "data/pharynx.csv", row.names = FALSE)
